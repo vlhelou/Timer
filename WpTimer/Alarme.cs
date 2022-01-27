9fa4ec020e4b3e612307fe069133f9d8582b55b9
vlhelou <vlhelou1971@gmail.com>
2022-01-27 00:30:53 -0300

    WinForm

---
+++ WpTimer/Alarme.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpTimer
+{
+    internal record Alarme
+    {
+        public string Nome { get; set; } = string.Empty;
+        public int Duracao { get; set; }
+        public System.Timers.Timer Timer { get; set; }=new System.Timers.Timer();
+    }
+}
