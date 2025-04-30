3117050ff9c4ed4cbe7ad781658b28184dc70d79
VASSILI HELOU <VASSILI.HELOU@gruposantamartadf.com.br>
2025-04-30 14:40:54 -0300

    form top off

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,80 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtIntervalo = new System.Windows.Forms.TextBox();
-            this.btnAcao = new System.Windows.Forms.Button();
-            this.lbFalta = new System.Windows.Forms.Label();
-            this.chMudo = new System.Windows.Forms.CheckBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            txtIntervalo = new TextBox();
+            btnAcao = new Button();
+            lbFalta = new Label();
+            chMudo = new CheckBox();
+            button1 = new Button();
+            SuspendLayout();
             // 
             // txtIntervalo
             // 
-            this.txtIntervalo.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtIntervalo.Location = new System.Drawing.Point(2, 3);
-            this.txtIntervalo.Name = "txtIntervalo";
-            this.txtIntervalo.Size = new System.Drawing.Size(120, 61);
-            this.txtIntervalo.TabIndex = 1;
-            this.txtIntervalo.Text = "300";
-            this.txtIntervalo.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            txtIntervalo.Font = new Font("Segoe UI", 30F);
+            txtIntervalo.Location = new Point(2, 3);
+            txtIntervalo.Name = "txtIntervalo";
+            txtIntervalo.Size = new Size(120, 61);
+            txtIntervalo.TabIndex = 1;
+            txtIntervalo.Text = "300";
+            txtIntervalo.TextAlign = HorizontalAlignment.Right;
             // 
             // btnAcao
             // 
-            this.btnAcao.Location = new System.Drawing.Point(128, 3);
-            this.btnAcao.Name = "btnAcao";
-            this.btnAcao.Size = new System.Drawing.Size(58, 28);
-            this.btnAcao.TabIndex = 2;
-            this.btnAcao.Text = "Inicia";
-            this.btnAcao.UseVisualStyleBackColor = true;
-            this.btnAcao.Click += new System.EventHandler(this.btnAcao_Click);
+            btnAcao.Location = new Point(128, 3);
+            btnAcao.Name = "btnAcao";
+            btnAcao.Size = new Size(58, 28);
+            btnAcao.TabIndex = 2;
+            btnAcao.Text = "Inicia";
+            btnAcao.UseVisualStyleBackColor = true;
+            btnAcao.Click += btnAcao_Click;
             // 
             // lbFalta
             // 
-            this.lbFalta.AutoSize = true;
-            this.lbFalta.Font = new System.Drawing.Font("Segoe UI", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lbFalta.Location = new System.Drawing.Point(256, 2);
-            this.lbFalta.Name = "lbFalta";
-            this.lbFalta.Size = new System.Drawing.Size(84, 62);
-            this.lbFalta.TabIndex = 3;
-            this.lbFalta.Text = "---";
-            this.lbFalta.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            lbFalta.AutoSize = true;
+            lbFalta.Font = new Font("Segoe UI", 35F);
+            lbFalta.Location = new Point(256, 2);
+            lbFalta.Name = "lbFalta";
+            lbFalta.Size = new Size(84, 62);
+            lbFalta.TabIndex = 3;
+            lbFalta.Text = "---";
+            lbFalta.TextAlign = ContentAlignment.MiddleRight;
             // 
             // chMudo
             // 
-            this.chMudo.AutoSize = true;
-            this.chMudo.Location = new System.Drawing.Point(192, 9);
-            this.chMudo.Name = "chMudo";
-            this.chMudo.Size = new System.Drawing.Size(58, 19);
-            this.chMudo.TabIndex = 4;
-            this.chMudo.Text = "Mudo";
-            this.chMudo.UseVisualStyleBackColor = true;
+            chMudo.AutoSize = true;
+            chMudo.Location = new Point(192, 9);
+            chMudo.Name = "chMudo";
+            chMudo.Size = new Size(58, 19);
+            chMudo.TabIndex = 4;
+            chMudo.Text = "Mudo";
+            chMudo.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(128, 34);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(58, 28);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Auto";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.btnAutomatico_Click);
+            button1.Location = new Point(128, 34);
+            button1.Name = "button1";
+            button1.Size = new Size(58, 28);
+            button1.TabIndex = 5;
+            button1.Text = "Auto";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += btnAutomatico_Click;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(346, 70);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.chMudo);
-            this.Controls.Add(this.lbFalta);
-            this.Controls.Add(this.btnAcao);
-            this.Controls.Add(this.txtIntervalo);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.TopMost = true;
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(346, 70);
+            Controls.Add(button1);
+            Controls.Add(chMudo);
+            Controls.Add(lbFalta);
+            Controls.Add(btnAcao);
+            Controls.Add(txtIntervalo);
+            Name = "Form1";
+            Text = "Timer";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
 
         }
 
